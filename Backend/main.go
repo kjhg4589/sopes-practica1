89a3d9f29b7be017e9c427104c6aa7da3d007d26
Kevin <kjhg.45.89@gmail.com>
2022-02-17 21:10:54 -0600

    Commit Inicial

---
+++ Backend/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Operacion struct {
+	Num1      float64 `json:"numero1,omitempty"`
+	FirstName string  `json:"firstname,omitempty"`
+	LastName  string  `json:"lastname,omitempty"`
+}
+
+var opers []Operacion
+
+func executeOper(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func returnReport(w http.ResponseWriter, req *http.Request) {
+	json.NewEncoder(w).Encode(opers)
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	// adding example data
+	opers = append(opers, Operacion{Num1: 1.2, FirstName: "Ryan", LastName: "Ray"})
+	opers = append(opers, Operacion{Num1: 2.2, FirstName: "Maria", LastName: "Ray"})
+
+	//endpoints
+	router.HandleFunc("/oper", executeOper).Methods("POST")
+	router.HandleFunc("/oper/report", returnReport).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":9081", router))
+}
