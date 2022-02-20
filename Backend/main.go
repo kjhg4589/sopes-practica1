de4a449af70d6497ab5cdcbc5f286d84aa9f2d4c
Kevin <kjhg.45.89@gmail.com>
2022-02-19 21:53:02 -0600

    Se agrega metodo para insertar en bd mongo

--- Backend/main.go
+++ Backend/main.go
@@ -4,11 +4,47 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
+
+	m "sopes-practica1/models"
+	historialService "sopes-practica1/service"
 
 	"github.com/gorilla/mux"
-
-	historialService "sopes-practica1/service"
 )
+
+func Operarar(n1 float64, n2 float64, op string) float64 {
+	switch op {
+	case "+":
+		return n1 + n2
+	case "-":
+		return n1 - n2
+	case "*":
+		return n1 * n2
+	case "/":
+		if n2 == 0 {
+			return 0
+		}
+		return n1 / n2
+	default:
+		return 0
+	}
+}
+
+func ExecuteOper(w http.ResponseWriter, req *http.Request) {
+	var historial m.Historial
+	_ = json.NewDecoder(req.Body).Decode(&historial)
+
+	historial.Resultado = Operarar(historial.Numero1, historial.Numero2, historial.Operacion)
+	historial.Fecha = time.Now()
+
+	err := historialService.Create(historial)
+
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+	}
+
+	json.NewEncoder(w).Encode(historial)
+}
 
 func ReturnReport(w http.ResponseWriter, req *http.Request) {
 	historial, err := historialService.Read()
@@ -23,6 +59,7 @@ func ReturnReport(w http.ResponseWriter, req *http.Request) {
 func main() {
 	router := mux.NewRouter()
 
+	router.HandleFunc("/oper", ExecuteOper).Methods("POST")
 	router.HandleFunc("/oper/report", ReturnReport).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":9090", router))
